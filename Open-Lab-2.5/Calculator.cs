a00ad05c5796f1e988107a0fed7caf469a430380
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-28 09:50:10 +0100

    Add project files.

---
+++ Open-Lab-2.5/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_2._5
+{
+    class Calculator
+    {
+        public bool DivisibleByFive (int number)
+        {
+            if (number % 5 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Open-Lab-2.5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Open_Lab_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator number = new Calculator();
+            Console.WriteLine(number.DivisibleByFive(865));
+
+                 
+        }
+    }
+}
